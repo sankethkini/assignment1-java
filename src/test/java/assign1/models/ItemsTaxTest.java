e1e9cc1bb1397181d63d2af2acf06154d5bb63f0
sankethkini <kinisanketh@gmail.com>
2022-03-29 19:13:39 +0530

    some more tests

--- src/test/java/assign1/models/ItemsTaxTest.java
+++ src/test/java/assign1/models/ItemsTaxTest.java
@@ -1,4 +1,16 @@
 package assign1.models;
 
+import assign1.exceptions.InvalidItemInput;
+import assign1.utils.CreateItem;
+import assign1.utils.ValidateItem;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ItemsTaxTest {
+    @Test
+    @DisplayName("check total price of raw")
+    public void testRawTax(){
+
+    }
 }
--- src/test/java/assign1/utils/CreateItemTest.java
+++ src/test/java/assign1/utils/CreateItemTest.java
@@ -1,8 +1,46 @@
 package assign1.utils;
 
+import assign1.exceptions.InvalidItemInput;
+import assign1.models.Item;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CreateItemTest {
+    @Test
+    @DisplayName("check if type is raw")
+    public void testTypeRaw(){
+        Assertions.assertDoesNotThrow( ()->{
+                     CreateItem.create("some","raw",(float)100.0,12);
+                }
+        );
+    }
+    @Test
+    @DisplayName("check if type is manufactured")
+    public void testTypeManufactured(){
+        Assertions.assertDoesNotThrow( ()->{
+                    CreateItem.create("some","manufactured",(float)100.0,12);
+                }
+        );
+    }
+    @Test
+    @DisplayName("check if type is imported")
+    public void testTypeImported(){
+        Assertions.assertDoesNotThrow( ()->{
+                    CreateItem.create("some","imported",(float)100.0,12);
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("check if type is wrong")
+    public void testWrongType(){
+        Assertions.assertThrows(
+                InvalidItemInput.class
+                ,()->{
+                    CreateItem.create("some","some",(float)100.0,12);
+                }
+        );
+    }
 
 }
